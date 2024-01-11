7a0cb8814d1903f639303a1f7f85e265bf479537
Gabriel <guardavege@gmail.com>
2024-01-11 10:32:21 -0300

    Tabelas Relacionadas

---
+++ src/migrations/20240111130800-create-enrollment.js
@@ -0,0 +1,38 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('inscriptions', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      status: {
+        type: Sequelize.BOOLEAN
+      },
+      course_id: {
+        allowNull : false,
+        type : Sequelize.INTEGER,
+        references : {model : 'courses', Key : 'id'}
+      },
+      student_id : {
+        allowNull : false,
+        type : Sequelize.INTEGER,
+        references : { model: 'persons', Key : 'id'}
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('inscriptions');
+  }
+};
