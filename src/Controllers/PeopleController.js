619fe83ae717aa5ed755bc8610a2b18e60dc2c3e
Gabriel <guardavege@gmail.com>
2024-01-11 08:58:46 -0300

    Adicionando Metodos HTTP

--- src/Controllers/PeopleController.js
+++ src/Controllers/PeopleController.js
@@ -1,10 +1,13 @@
+const database = require('../models');
+
 class PeopleController
 {
   static async GetAll(req,res)
   {
     try
     {
-      ///
+      const people = await database.People.findAll();
+      return res.status(200).json(people);
     }catch (error)
     {
       ///
