3c190ef3d3ae4c4a97dbeedd370a0b78fe6608b8
Gabriel <126411934+Gabrielbprado@users.noreply.github.com>
2024-11-28 18:03:08 -0300

    Update personFactory.js

--- Api/src/tests/factories/personFactory.js
+++ Api/src/tests/factories/personFactory.js
@@ -4,7 +4,7 @@ const { faker } = require("@faker-js/faker");
 const generatePerson = () => {
     return {
       name: faker.person.fullName(),
-      cpf: faker.number.int({ min: 10000000000, max: 99999999999 }),  // Substituindo a função errada
+      cpf: faker.number.int({ min: 10000000000, max: 99999999999 }), 
       ativo: true,
       createdAt: new Date(),
       updatedAt: new Date(),
